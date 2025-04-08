95239642dc522cf92b1f3819f3391abc5454281c
NoorUllah43 <noorullah382040.com>
2025-04-08 17:36:46 +0500

    paragraph count add

--- main.go
+++ main.go
@@ -10,138 +10,195 @@ func main() {
 	if ferr != nil {
 		panic(ferr)
 	}
-	
+
 	str := string(file)
 
 	numCharacter(str)
-	numWord(str)
 	numLines(str)
-	numSpaces(str)
 	numVowels(str)
 	numDigits(str)
 	numSpecialCharacter(str)
 	numSymboles(str)
 	numPunctuation(str)
-	
+	numWord(str)
+	numSpaces(str)
+	numParagraph(str)
+	combineFunc(str)
 
 }
 
-
-
-
-
-// Number of character
-func numCharacter(str string)  {
+// Number of character -----------------------------------------------------------------------
+func numCharacter(str string) {
 	character := 0
 	for i := 0; i < len(str); i++ {
 		character++
 	}
-	fmt.Printf("Number of character: %v\n",character)
-	
+	fmt.Printf("Number of character: %v\n", character)
+
 }
 
-// Number of lines
-func numLines(str string)  {
+// Number of lines----------------------------------------------------------------------------
+func numLines(str string) {
 	Lines := 0
-	for _,ch := range str {
-		if ch == rune('\n') {
+	for _, ch := range str {
+		if ch == rune('.') {
 			Lines++
 		}
-		
+
 	}
-	fmt.Printf("Number of Lines: %v\n",Lines)
-	
+	fmt.Printf("Number of Lines: %v\n", Lines)
+
 }
 
-// Number of vowels
-func numVowels(str string)  {
+// Number of vowels----------------------------------------------------------------------------
+func numVowels(str string) {
 	vowels := 0
-	for _,ch := range str {
+	for _, ch := range str {
 		if ch == rune('a') || ch == rune('e') || ch == rune('i') || ch == rune('o') || ch == rune('u') {
 			vowels++
 		}
-		
+
 	}
-	fmt.Printf("Number of vowels: %v\n",vowels)
-	
+	fmt.Printf("Number of vowels: %v\n", vowels)
+
 }
 
-// Number of digits
-func numDigits(str string)  {
+// Number of digits---------------------------------------------------------------------------
+func numDigits(str string) {
 	digits := 0
-	for _,ch := range str {
+	for _, ch := range str {
 		if ch == rune('0') || ch == rune('1') || ch == rune('2') || ch == rune('3') || ch == rune('4') || ch == rune('5') || ch == rune('6') || ch == rune('7') || ch == rune('8') || ch == rune('9') {
 			digits++
 		}
-		
+
 	}
-	fmt.Printf("Number of digits: %v\n",digits)
-	
+	fmt.Printf("Number of digits: %v\n", digits)
+
 }
 
-// Number of SpecialCharacter
-func numSpecialCharacter(str string)  {
+// Number of SpecialCharacter----------------------------------------------------------------
+func numSpecialCharacter(str string) {
 	SpecialCharacter := 0
-	for _,ch := range str {
+	for _, ch := range str {
 		if ch == rune('!') || ch == rune('@') || ch == rune('#') || ch == rune('$') || ch == rune('%') || ch == rune('^') || ch == rune('&') || ch == rune('*') || ch == rune('~') {
 			SpecialCharacter++
 		}
-		
+
 	}
-	fmt.Printf("Number of SpecialCharacter: %v\n",SpecialCharacter)
-	
+	fmt.Printf("Number of SpecialCharacter: %v\n", SpecialCharacter)
+
 }
 
-
-// Number of symbol
-func numSymboles(str string)  {
+// Number of symbol---------------------------------------------------------------------------------
+func numSymboles(str string) {
 	symboles := 0
-	for _,ch := range str {
-		if ch == rune('[') || ch == rune(']') || ch == rune('{') || ch == rune('}') || ch == rune('(') || ch == rune(')') || ch == rune('|') || ch == rune('\\') || ch == rune('/') || ch == rune('+') || ch == rune('=') || ch == rune('<') || ch == rune('>'){
+	for _, ch := range str {
+		if ch == rune('[') || ch == rune(']') || ch == rune('{') || ch == rune('}') || ch == rune('(') || ch == rune(')') || ch == rune('|') || ch == rune('\\') || ch == rune('/') || ch == rune('+') || ch == rune('=') || ch == rune('<') || ch == rune('>') || ch == rune('_') {
 			symboles++
 		}
-		
+
 	}
-	fmt.Printf("Number of SpecialCharacter: %v\n",symboles)
-	
+	fmt.Printf("Number of symboles: %v\n", symboles)
+
 }
 
-// Number of symbol
-func numPunctuation(str string)  {
+// Number of punctuation---------------------------------------------------------------------------------
+func numPunctuation(str string) {
 	punctuation := 0
-	for _,ch := range str {
-		if ch == rune(';') || ch == rune(':') || ch == rune('\'') || ch == rune('"') || ch == rune(',') || ch == rune('.') || ch == rune('?') {
+	for _, ch := range str {
+		if ch == rune(';') || ch == rune(':') || ch == rune('\'') || ch == rune('"') || ch == rune(',') || ch == rune('.') || ch == rune('?') || ch == rune('-') {
 			punctuation++
 		}
-		
+
 	}
-	fmt.Printf("Number of SpecialCharacter: %v\n",punctuation)
-	
+	fmt.Printf("Number of punctuation: %v\n", punctuation)
+
 }
 
-
-// Number of word
-func numWord(str string)  {
+// Number of word---------------------------------------------------------------------------------
+func numWord(str string) {
 	words := 0
-	for _,ch := range str {
+	for _, ch := range str {
 		if ch == rune(' ') || ch == rune('\n') {
 			words++
 		}
-		
+
 	}
-	fmt.Printf("Number of words: %v\n",words)
-	
+	fmt.Printf("Number of words: %v\n", words)
+
 }
 
-// Number of spaces
-func numSpaces(str string)  {
+// Number of spaces-----------------------------------------------------------------------------
+func numSpaces(str string) {
 	spaces := 0
-	for _,ch := range str {
+	for _, ch := range str {
 		if ch == rune(' ') {
 			spaces++
 		}
-		
+
 	}
-	fmt.Printf("Number of spaces: %v\n",spaces)
+	fmt.Printf("Number of spaces: %v\n", spaces)
+
+}
+
+// // Number of paragraph---------------------------------------------------------------------------
+func numParagraph(str string) {
+	paragraph := 0
+	for _, ch := range str {
+		if ch == rune('\n') {
+			paragraph++
+		}
+
+	}
+	fmt.Printf("Number of paragraph: %v\n", paragraph)
+
+}
+
+// ------------------------------------------------------------------------------------------------
+
+func combineFunc(str string) {
+	data := make(map[string]int)
+	data["lines"] = 0
+	data["vowels"] = 0
+	data["digits"] = 0
+	data["specialCharacters"] = 0
+	data["symboles"] = 0
+	data["punctuation"] = 0
+	data["words"] = 0
+	data["spaces"] = 0
+	data["paragraph"] = 0
+
 	
+	for _, ch := range str {
+
+		if ch == rune(' ') {
+			data["spaces"]++
+		}
+		if ch == rune(' ') || ch == rune('\n') {
+			data["words"]++
+		}
+		if ch == rune(';') || ch == rune(':') || ch == rune('\'') || ch == rune('"') || ch == rune(',') || ch == rune('.') || ch == rune('?') {
+			data["punctuation"]++
+		}
+		if ch == rune('[') || ch == rune(']') || ch == rune('{') || ch == rune('}') || ch == rune('(') || ch == rune(')') || ch == rune('|') || ch == rune('\\') || ch == rune('/') || ch == rune('+') || ch == rune('=') || ch == rune('<') || ch == rune('>') {
+			data["symboles"]++
+		}
+		if ch == rune('!') || ch == rune('@') || ch == rune('#') || ch == rune('$') || ch == rune('%') || ch == rune('^') || ch == rune('&') || ch == rune('*') || ch == rune('~') {
+			data["specialCharacters"]++
+		}
+		if ch == rune('0') || ch == rune('1') || ch == rune('2') || ch == rune('3') || ch == rune('4') || ch == rune('5') || ch == rune('6') || ch == rune('7') || ch == rune('8') || ch == rune('9') {
+			data["digits"]++
+		}
+		if ch == rune('a') || ch == rune('e') || ch == rune('i') || ch == rune('o') || ch == rune('u') {
+			data["vowels"]++
+		}
+		if ch == rune('.') {
+			data["lines"]++
+		}
+		if ch == rune('\n') {
+			data["paragraph"]++
+		}
+	}
+
+	fmt.Println(data)
 }
